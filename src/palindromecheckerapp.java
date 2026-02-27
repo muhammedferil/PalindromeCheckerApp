61805eacb242c0ac687c912cf838dd2656963ecb
muhammedferil <muhammedferil45@gmail.com>
2026-02-27 16:44:38 +0530

    UC7: Optimized palindrome check using Deque

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,36 +1,31 @@
 import java.util.Scanner;
-import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class palindromecheckerapp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
+        Deque<Character> deque = new ArrayDeque<>();
 
         System.out.println("Enter a string:");
         String input = scanner.nextLine();
 
-        // Insert characters into Stack and Queue
+        // Insert characters into Deque
         for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            stack.push(ch);      // LIFO
-            queue.add(ch);       // FIFO
+            deque.addLast(input.charAt(i));
         }
 
         boolean isPalindrome = true;
 
-        // Compare dequeue (FIFO) with pop (LIFO)
-        for (int i = 0; i < input.length(); i++) {
+        // Compare front and rear
+        while (deque.size() > 1) {
 
-            char fromQueue = queue.poll();  // FIFO
-            char fromStack = stack.pop();   // LIFO
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
 
-            if (fromQueue != fromStack) {
+            if (front != rear) {
                 isPalindrome = false;
                 break;
             }
